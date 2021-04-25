dfe3bfed241790379d0fff385fe0e3e0f7b25e60
Memo Chou <memochou1993@gmail.com>
2021-04-25 23:38:16 +0800

    Add Gtag dependency

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue';
 import { Quasar } from 'quasar';
+import VueGtag from 'vue-gtag-next';
 import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
@@ -10,4 +11,9 @@ createApp(App)
   .use(Quasar, quasarUserOptions)
   .use(store)
   .use(router)
+  .use(VueGtag, {
+    property: {
+      id: 'G-J7M1ZGEG4Q',
+    },
+  })
   .mount('#app');
